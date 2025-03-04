68b3b0144ebd40c8328d47b1868f714e201eab0d
Thealexsidney <Thealexsidney@gmail.com>
2025-03-03 18:45:25 -0600

    Revert "fails"
    This reverts commit 025bfb12e07e32b22ede517cd94a96f3cb80ebef.

--- Dodge and Collect/Assets/Scripts/Laser.cs
+++ Dodge and Collect/Assets/Scripts/Laser.cs
@@ -1,44 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class Laser : MonoBehaviour
 {
     public float laserLife = 5;
     public GameObject laserEmmiter;
-    public float edgeMargin = 1f;
+
     private float timer = 0;
     private LineRenderer lineRenderer;
-    private EdgeCollider2D edgeCollider;
-
+    
     // Start is called before the first frame update
     void Start()
     {
 
         
-        edgeCollider = GetComponent<EdgeCollider2D>();
-        lineRenderer = GetComponent<LineRenderer>();
-
         
+        lineRenderer = GetComponent<LineRenderer>();
 
         Vector2 pointA = RandomPointOnScreen();
         Vector2 pointB = RandomPointOnScreen();
 
-        Vector2 localPointA = transform.InverseTransformPoint(pointA);
-        Vector2 localPointB = transform.InverseTransformPoint(pointB);
-
         lineRenderer.SetPosition(0, pointA);
         lineRenderer.SetPosition(1, pointB);
-
-        edgeCollider.points = new Vector2[]
-        {
-            new Vector2(pointA.x, pointA.y),
-            new Vector2(pointB.x, pointB.y)
-        };
-
-
-        
 
         Instantiate(laserEmmiter, pointA, Quaternion.identity);
         Instantiate(laserEmmiter, pointB, Quaternion.identity);
@@ -52,16 +36,13 @@ public class Laser : MonoBehaviour
         timer += Time.deltaTime;
     }
 
-    
-
     Vector3 RandomPointOnScreen()
     {
-        
         float randomX = Random.Range(0, Screen.width);
         float randomY = Random.Range(0, Screen.height);
 
-        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(randomX, randomY, Camera.main.nearClipPlane + 5f));
-        worldPosition.z = 0f;
+        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(new Vector2(randomX, randomY));
+
         return worldPosition;
     }
 }
